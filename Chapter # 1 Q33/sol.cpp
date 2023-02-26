c37eaaf5cced0091c750ecc2d6b2fc8e3982452b
Malik Moeez Nawaz <malikmoeez508@gmail.com>
2023-02-26 14:48:44 +0500

    first commit

---
+++ Chapter # 1 Q33/sol.cpp
@@ -0,0 +1,17 @@
+// problem # 33
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+int x=0,y=5,z;
+x=x+1;
+y=y+1;
+z=x+y;
+
+y=z;
+cout << "Total is : " << y;
+
+return 0;
+}
